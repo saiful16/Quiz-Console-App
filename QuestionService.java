52f0d9a31dbc08abffcda68ae4a20fcca36bebe8
Saiful <saifulislam.cse164@gmail.com>
2024-11-19 00:31:41 +0100

    question services are added

--- QuestionService.java
+++ QuestionService.java
@@ -2,11 +2,11 @@ public class QuestionService {
     Question[] questions = new Question[5];
 
     public QuestionService(){
-        questions[0] = new Question(1, "What", "java", "cpp", "python", "JS", "java");
-        questions[1] = new Question(1, "What", "java", "cpp", "python", "JS", "java");
-        questions[2] = new Question(1, "What", "java", "cpp", "python", "JS", "java");
-        questions[3] = new Question(1, "What", "java", "cpp", "python", "JS", "java");
-        questions[4] = new Question(1, "What", "java", "cpp", "python", "JS", "java");
+        questions[0] = new Question(1, "size of integer", "2", "6", "4", "8", "4");
+        questions[1] = new Question(2, "size of double", "2", "6", "4", "8", "8");
+        questions[2] = new Question(3, "size of char", "2", "6", "4", "8", "2");
+        questions[3] = new Question(4, "size of long", "2", "6", "4", "8", "8");
+        questions[4] = new Question(5, "size of boolean", "1", "2", "4", "8", "1");
     }
 
     public void displayQuestion(){
